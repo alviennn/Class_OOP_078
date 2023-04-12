46d7c62394945c8a4cb805f00191e2a41f253317
alviennn <alvien.rnp@gmail.com>
2023-04-12 10:36:03 +0700

    memasukkan variable private dan input  public

--- BelajarOOP/BelajarOOP.cpp
+++ BelajarOOP/BelajarOOP.cpp
@@ -6,4 +6,12 @@ private:
 	int nim;
 	string nama;
 	float nilai;
+public:
+	void input() {
+		cout << "Masukan NIM :";
+		cin >> nim;
+		cout << "Masukan Nama :";
+		cin >> nama;
+		cout << "Masukan Nilai :";
+	}
 };
